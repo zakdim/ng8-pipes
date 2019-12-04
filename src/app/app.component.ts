1ffc14069178b3e19ad6541d7ae221d53d645dc4
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-12-04 00:23:21 -0500

    Pure and impure pipes
    section 17, lecture 247

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,7 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
-  
+
   filteredStatus = '';
 
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
@@ -41,5 +41,14 @@ export class AppComponent {
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)      
+    });
   }
 }
